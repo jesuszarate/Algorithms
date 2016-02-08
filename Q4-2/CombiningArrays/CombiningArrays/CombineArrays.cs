bf06841bb5f3a6179f81dabc0102718205ce811e
Jesus <chuy8jay@gmail.com>
2016-02-07 23:02:43 -0700

    Added more steps and added some values

--- Q4-2/CombiningArrays/CombiningArrays/CombineArrays.cs
+++ Q4-2/CombiningArrays/CombiningArrays/CombineArrays.cs
@@ -10,45 +10,75 @@ namespace CombiningArrays
     {
         // A and B are each sorted into ascending order, and 0 <= k < |A|+|B| 
         // Returns the element that would be stored at index k if A and B were
-// combined into a single array that was sorted into ascending order.
-public int select (int[] A, int[] B, int k){
-    //return select(A, 0, |A|-1, B, 0, |B|-1, k);
-    return select(A, 0, A.Length-1, B, 0, B.Length-1, k);
+        // combined into a single array that was sorted into ascending order.
+        public int select(int[] A, int[] B, int k)
+        {
+            //return select(A, 0, |A|-1, B, 0, |B|-1, k);
+            return select(A, 0, A.Length - 1, B, 0, B.Length - 1, k);
 
-}
+        }
 
-public int select(int[] A, int loA, int hiA, int[] B, int loB, int hiB, int k)
-{
-    // A[loA..hiA] is empty
-    if (hiA < loA)
-        return B[k-loA];
 
-    // B[loB..hiB] is empty
-    if (hiB < loB)
-        return A[k-loB];
-    // Get the midpoints of A[loA..hiA] and B[loB..hiB]
-    int i = (loA + hiA)/2;
-    int j = (loB+hiB)/2;
-    // Figure out which one of four cases apply
-    if (A[i] <= B[j]){
-        if (k > i+j){
-            //return select(A, $1, $2, B, $3, $4, k);
-            return select(A, 1, 2, B, 3, 4, k);
+        // 3 len = 1
+        // 2 len = 1
+        // k = 2
+        public int select(int[] A, int loA, int hiA, int[] B, int loB, int hiB, int k)
+        {
+            // A[loA..hiA] is empty
+            if (hiA < loA)
+                return B[k - loA];
+
+            // B[loB..hiB] is empty
+            if (hiB < loB)
+                return A[k - loB];
+
+            // Get the midpoints of A[loA..hiA] and B[loB..hiB]
+            int midA = (loA + hiA) / 2; // 1 / 2 = 0
+            int midB = (loB + hiB) / 2; // 1 / 2 = 0
+
+            // Figure out which one of four cases apply
+            if (A[midA] <= B[midB]) // If A's midpoint element is less than B's
+            {
+                // Advance the start of A by one position
+                //return select(A, loA+1, hiA, B, loB, hiB, k);
+
+
+                // If k is greater than the mid, we won't need anything before 
+                // the midpoint
+                if (k > midA + midB) // is 8 > 0 -> yes
+                // is 1 > 1+1 -> no
+                {
+                    //return select(A, $1, $2, B, $3, $4, k);
+                    //return select(A, midA + 1, hiA, B, midB, hiB, k);
+                    return select(A, loA, midA - 1, B, midB, hiB, k);
+
+                }
+                else // k is less than mid, so we won't need anything after midpoint
+                {
+                    //return select(A, $5, %6, B, $7, $8, k);
+                    return select(A, midA + 1, hiA, B, midB, hiB, k);
+
+                }
+            }
+            else // If B's midpoint is less than A's
+
+                // Advance the start of B by one
+                //return select(A, loA, hiA, B, loB+1, hiB, k);
+
+                if (k > midA + midB)
+                {
+                    //return select(A, $9, $10, B, $11, $12, k);
+                    //return select(A, midA, hiA, B, midB + 1, hiB, k);
+                    return select(A, midA, hiA, B, loB, midB - 1, k);
+
+                }
+                else
+                {
+                    //return select(A, $13, $14, B, $15, $16, k);
+                    //return select(A, loA, midA, B, loB + 1, midB, k);
+                    return select(A, loA, midA, B, midB + 1, hiA, k);
+
+                }
         }
-        else{
-            //return select(A, $5, %6, B, $7, $8, k);
-            return select(A, 5, 6, B, 7, 8, k);
-        }
-    }
-    else
-        if (k > i+j){
-            //return select(A, $9, $10, B, $11, $12, k);
-            return select(A, 9, 10, B, 11, 12, k);
-        }
-        else{
-            //return select(A, $13, $14, B, $15, $16, k);
-            return select(A, 13, 14, B, 15, 16, k);
-        }
-}
     }
 }
--- Q4-2/CombiningArrays/CombiningArrays/Program.cs
+++ Q4-2/CombiningArrays/CombiningArrays/Program.cs
@@ -12,23 +12,117 @@ namespace CombiningArrays
         {
             CombineArrays ca = new CombineArrays();
 
-            // Test 1
-            int[] A = {3,5,6};
-            int[] B = {2,8,9};
-            int k = 8;
+            int[] A;
+            int[] B;
+            int k;
+            //// Test 1
+            int Test = 1;
+            A = new int[] { 3, 6, 7 };
+            B = new int[] { 2, 8, 9 };
+            k = 0;
 
-            ca.select(A, B, k);
+            int ans = 2;
+            int res = ca.select(A, B, k);
+            TestResult(res, ans, Test);
 
             // Test 2
+            Test++;
+            k = 5;
+
+            ans = 9;
+            res = ca.select(A, B, k);
+            TestResult(res, ans, Test);
+
+
+            //// Test 3
+            Test++;
+            A = new int[] { 3, 5, 6 };
+            B = new int[] { 2, 8, 9, 10 };
+
+            // A + B = 2, 3, 5, 6, 8, 9, 10
+
             k = 2;
 
-            ca.select(A, B, k);
+            ans = 5;
+            res = ca.select(A, B, k);
+            TestResult(res, ans, Test);
+            
+            // Test 4
+            Test++;
+            A = new int[] { 1, 2, 3, 4};
+            B = new int[] { 3, 5, 6, 8, 9 };
 
-            // Test 3
-            A = new int[]{ 3, 5, 6 };
-            B = new int[]{ 2, 8, 9 };
+            // A + B = 1,2,3,3,4,5,6,8,9
 
-            k = 2;
+            Console.WriteLine();
+            Console.WriteLine("A = new int[] { 1, 2, 3, 4};");
+            Console.WriteLine("B = new int[] { 3, 5, 6, 8, 9 };");
+            Console.WriteLine();
+            Console.WriteLine("0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 ");
+            Console.WriteLine("1 | 2 | 3 | 3 | 4 | 5 | 6 | 8 | 9 ");
+            Console.WriteLine();
+
+            k = 3;
+
+            ans = 3;
+            res = ca.select(A, B, k);
+            TestResult(res, ans, Test);
+
+            // Test 5
+            Test++;
+            k = 7;
+
+            ans = 8;
+            res = ca.select(A, B, k);
+            TestResult(res, ans, Test);
+
+            // Test 6
+            Test++;
+            k = 4;
+
+            ans = 4;
+            res = ca.select(A, B, k);
+            TestResult(res, ans, Test);
+
+            // Test 7
+            Test++;
+            k = 3;
+
+            ans = 2;
+            res = ca.select(A, B, k);
+            TestResult(res, ans, Test);
+
+            // Test 
+            Test++;
+            A = new int[] { 2 };
+            B = new int[] { 3 };
+
+            k = 1;
+
+            ans = 3;
+            res = ca.select(A, B, k);
+            TestResult(res, ans, Test);
+
+            // Test 
+            Test++;
+            A = new int[] { 2 };
+            B = new int[] { 3 };
+
+            k = 1;
+
+            ans = 3;
+            res = ca.select(A, B, k);
+            TestResult(res, ans, Test);
+            Console.Read();
+        }
+
+        public static void TestResult(int results, int actualRes, int tstNumber)
+        {
+            if (results == actualRes)
+                Console.WriteLine("Test " + tstNumber + ": passed - Answer: " + results);
+            else
+                Console.WriteLine("Test " + tstNumber + ": failed - Result: " + results + " should be " + actualRes);
+
         }
     }
 }
